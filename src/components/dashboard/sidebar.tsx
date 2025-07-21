fbb48cffec5ea8dd2a728c1cae3928e9b7dddb91
Brian Min <bymi15@yahoo.com>
2025-07-21 17:25:14 +0100

    remove grayscale

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -200,7 +200,7 @@ export function NavUser({ user }: { user: User }) {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg grayscale">
+              <Avatar className="h-8 w-8 rounded-lg">
                 <AvatarImage src={avatarUrl} alt="avatar" />
                 <AvatarFallback className="rounded-lg">AB</AvatarFallback>
               </Avatar>
